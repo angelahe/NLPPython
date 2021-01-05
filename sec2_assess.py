327a685e816d2a946ddb213ac5b067c3318a3932
angela <angela@aviak.ca>
2021-01-04 17:24:11 -0700

    assessment start

---
+++ sec2_assess.py
@@ -0,0 +1,15 @@
+# 1. print NLP stands for Natural Language Processing using variables
+abbr = 'NLP'
+full_text = 'Natural Language Processing'
+print(f'{abbr} stands for {full_text}')
+
+# 2. create a file in the current working directory called contacts.txt
+
+# 3. Open the file and use .read() to save the contents of the file to a string called `fields`.  Make sure the file is closed at the end.
+
+# 4. Use PyPDF2 to open the file `Business_Proposal.pdf`. Extract the text of page 2.
+
+# 5. Open the file `contacts.txt` in append mode. Add the text of page 2 from above to `contacts.txt`.
+#  CHALLENGE: See if you can remove the word \"AUTHORS:\"
+
+# 6. Using the `page_two_text` variable created above, extract any email addresses that were contained in the file `Business_Proposal.pdf`."
