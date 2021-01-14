79acfbb0ca3e5e07d7cdfd497f0ccb4159081925
angela <angela@aviak.ca>
2021-01-13 20:45:15 -0700

    matching phrases

--- S03_vocab_matching.py
+++ S03_vocab_matching.py
@@ -41,3 +41,24 @@ matcher.add('SolarPower', None, pattern4, pattern5)
 doc2 = nlp(u"Solar--Power is solarpower. The Solar Power industry continues to grow a solarpower increases. Solar-power is awesome.")
 found_matches2 = matcher(doc2)
 print(found_matches2)
+
+#now phrase matching
+from spacy.matcher import PhraseMatcher
+
+matcher3 = PhraseMatcher(nlp.vocab)
+# material/UPDATED_NLP_COURSE/TextFiles/reaganomics.txt
+with open('material/UPDATED_NLP_COURSE/TextFiles/reaganomics.txt') as f:
+    doc3 = nlp(f.read())
+
+phrase_list = ['voodoo economics', 'supply-side economics', 'trickle-down economics', 'free-market economics']
+# do list comprehension to gather phrases
+phrase_patterns = [nlp(text) for text in phrase_list]
+# pass each doc pattern into matcher
+matcher3.add('EconMatcher', None, *phrase_patterns)
+found_matches3 = matcher3(doc3)
+print(found_matches3)
+
+for match_id, start, end in found_matches3:
+    string_id = nlp.vocab.strings[match_id]
+    span = doc[start:end]
+    print(match_id, string_id, start, end, span.text)
