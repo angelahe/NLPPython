ec1fcfd9452bb7ef6ee22ac7c99335de454b0cf1
angela <angela@aviak.ca>
2021-01-14 20:17:17 -0700

    find phrases, print surrounding text, print sentences matches are in

--- S03_assess.py
+++ S03_assess.py
@@ -24,9 +24,30 @@ for token in doc_sentences[1]:
     print(f'{token.text:{15}} {token.pos_:{10}} {token.dep_:{10}} {token.lemma_}')
 
 # write a matcher called Swimming that finds swimming vigorously
+from spacy.matcher import Matcher
+matcher = Matcher(nlp.vocab)
+
 # with 'IS_SPACE' pattern between the two words
-# import matcher library
 # create pattern and add it to matcher
+pattern = [{'LOWER':'swimming'}, {'IS_SPACE':True, 'OP':'*'}, {'LOWER':'vigorously'} ]
+matcher.add('Swimming', None, pattern)
 # create a list of matches and print the list
+found_matches = matcher(doc)
+print(found_matches)
 # print the text surrounding each match
+def surrounding_text(doc, start, end):
+    print(doc[start-5:end+5])
+
+surrounding_text(doc, 1274, 1277)
+surrounding_text(doc, 3607, 3610)
+
 # print the sentence that contains each found match
+for sentence in doc_sentences:
+    if found_matches[0][1] < sentence.end:
+        print(sentence)
+        break
+
+for sentence in doc_sentences:
+    if found_matches[1][1] < sentence.end:
+        print(sentence)
+        break
