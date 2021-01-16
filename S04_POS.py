daea16592388fa48a040b375afe929825e0340b0
angela <angela@aviak.ca>
2021-01-16 11:34:22 -0700

    parts of speech coarse and fine grained

--- S04_POS.py
+++ S04_POS.py
@@ -14,4 +14,10 @@ import spacy
 nlp = spacy.load('en-core-web-sm')
 doc = nlp(u"The quick brown fox jumped over the lazy dog's back.")
 
-print(doc.text)
+print(f'the text: {doc.text}')
+print(f'the token: {doc[4].text}')
+print(f'the part of speech: {doc[4].pos_}')
+print(f'the fine grained part of speech: {doc[4].tag_}')
+
+for token in doc:
+    print(f"{token.text:{10}} {token.pos_:{10}} {token.tag_:{10}} {spacy.explain(token.tag_)}")
