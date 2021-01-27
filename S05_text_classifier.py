ba70ad50d7243407c830d9deea68af86e3524c9f
angela <angela@aviak.ca>
2021-01-26 19:42:01 -0700

    try naive bayes algorithm first

--- S05_text_classifier.py
+++ S05_text_classifier.py
@@ -24,8 +24,47 @@ print(f'empty is empty? {empty.isspace()}')
 blanks = []
 # index, label, review text
 for i, lb, rv in df.itertuples():
-    if rv.isspace():
-        blanks.append(i)
+    if type(rv) == str:     # avoid NaN values
+        if rv.isspace():
+            blanks.append(i)
 # now that have index positions of blank reviews can drop them
 df.drop(blanks, inplace=True)
 print(f'number of valid reviews is now: {len(df)}')
+
+# look at distribution
+print(df['label'].value_counts())
+
+# now split data into training set and test set
+from sklearn.model_selection import train_test_split
+
+X = df['review']
+y = df['label']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import LinearSVC
+
+# Naïve Bayes:
+text_clf_nb = Pipeline([('tfidf', TfidfVectorizer()),
+                        ('clf', MultinomialNB()),
+                        ])
+
+# Linear SVC:
+text_clf_lsvc = Pipeline([('tfidf', TfidfVectorizer()),
+                          ('clf', LinearSVC()),
+                          ])
+
+# try naive bayes first
+text_clf_nb.fit(X_train, y_train)
+# Form a prediction set
+predictions = text_clf_nb.predict(X_test)
+# Report the confusion matrix
+from sklearn import metrics
+print(metrics.confusion_matrix(y_test,predictions))
+# Print a classification report
+print(metrics.classification_report(y_test,predictions))
+# Print the overall accuracy
+print(f'overall accuracy: {metrics.accuracy_score(y_test,predictions)}')
