3839f31a8239dbaa6b026978f95965bc52db83ad
angela <angela@aviak.ca>
2021-01-28 20:33:22 -0700

    read in iris dataset, split into training and test, standardize the data, build the network with keras, train the model for 150 epochs

---
+++ S08_Keras.py
@@ -0,0 +1,78 @@
+# deep learning
+# activation functions:
+# sigmoid function
+# hyperbolic tangent
+# rectified linear unit(ReLU)
+
+import numpy
+
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+print(type(iris))
+
+# type of iris is: sklearn.utils.Bunch
+print(iris.DESCR)
+
+X = iris.data
+
+# X
+y = iris.target
+
+print(y)
+
+# class 0 --> [1.0.0]
+# class 1 --> [0,1,0]
+# class 2 --> [0,0,1]
+
+# transform each into a vector of 3
+from keras.utils import to_categorical
+y = to_categorical(y)
+print(y.shape)
+print(y.shape)
+
+# split the training data and test data
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+print(X_train)
+print(X_test)
+print(y_train)
+print(y_test)
+
+# note the data is shuffled for us so we're not training a bunch of 0's then suddenly hit the 1s
+
+#standardize the data
+from sklearn.preprocessing import MinMaxScaler
+
+scaler_object = MinMaxScaler()
+
+scaler_object.fit(X_train)
+
+scaled_X_train = scaler_object.transform(X_train)
+#note these are now all between 0 and 1
+print(scaled_X_train)
+scaled_X_test = scaler_object.transform(X_test)
+
+X_train.max()
+scaled_X_train.max()
+
+print(X_train)
+print(scaled_X_train)
+
+from keras.models import Sequential
+from keras.layers import Dense
+
+#may need to tweak the hyperparameters below
+
+model = Sequential()
+model.add(Dense(8, input_dim=4, activation='relu'))
+model.add(Dense(8, input_dim=4, activation='relu'))
+model.add(Dense(3, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+print(model.summary())
+
+# fit and train the model
+# Play around with number of epochs as well!
+print(model.fit(scaled_X_train,y_train,epochs=150, verbose=2))
+
