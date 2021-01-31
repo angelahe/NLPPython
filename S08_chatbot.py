11530be25fc2441694656d2c462e203385efdef0
angela <angela@aviak.ca>
2021-01-31 16:04:28 -0700

    vectorize the stories

--- S08_chatbot.py
+++ S08_chatbot.py
@@ -50,3 +50,85 @@ max_question_len = max([len(data[1]) for data in all_data])
 
 print(f'max question length: {max_question_len}')
 
+# vectorize the data
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
+
+# integer encode sequences of words
+tokenizer = Tokenizer(filters=[])
+tokenizer.fit_on_texts(vocab)
+
+print(f'tokenizer object: word number index and lowercased: {tokenizer.word_index}')
+
+train_story_text = []
+train_question_text = []
+train_answers = []
+
+for story,question,answer in train_data:
+    train_story_text.append(story)
+    train_question_text.append(question)
+    train_answers.append(answer)
+
+train_story_seq = tokenizer.texts_to_sequences(train_story_text)
+
+# train story test
+def vectorize_stories(data, word_index=tokenizer.word_index, max_story_len=max_story_len,max_question_len=max_question_len):
+    '''
+    INPUT:
+
+    data: consisting of Stories,Queries,and Answers
+    word_index: word index dictionary from tokenizer
+    max_story_len: the length of the longest story (used for pad_sequences function)
+    max_question_len: length of the longest question (used for pad_sequences function)
+
+
+    OUTPUT:
+
+    Vectorizes the stories,questions, and answers into padded sequences. We first loop for every story, query , and
+    answer in the data. Then we convert the raw words to an word index value. Then we append each set to their appropriate
+    output list. Then once we have converted the words to numbers, we pad the sequences so they are all of equal length.
+
+    Returns this in the form of a tuple (X,Xq,Y) (padded based on max lengths)
+    '''
+
+
+    # X = STORIES
+    X = []
+    # Xq = QUERY/QUESTION
+    Xq = []
+    # Y = CORRECT ANSWER
+    Y = []
+
+
+    for story, query, answer in data:
+
+        # Grab the word index for every word in story
+        x = [word_index[word.lower()] for word in story]
+        # Grab the word index for every word in query
+        xq = [word_index[word.lower()] for word in query]
+
+        # Grab the Answers (either Yes/No so we don't need to use list comprehension here)
+        # Index 0 is reserved so we're going to use + 1
+        y = np.zeros(len(word_index) + 1)
+
+        # Now that y is all zeros and we know its just Yes/No , we can use numpy logic to create this assignment
+        #
+        y[word_index[answer]] = 1
+
+        # Append each set of story,query, and answer to their respective holding lists
+        X.append(x)
+        Xq.append(xq)
+        Y.append(y)
+
+    # Finally, pad the sequences based on their max length so the RNN can be trained on uniformly long sequences.
+
+    # RETURN TUPLE FOR UNPACKING
+    return (pad_sequences(X, maxlen=max_story_len),pad_sequences(Xq, maxlen=max_question_len), np.array(Y))
+
+inputs_train, queries_train, answers_train = vectorize_stories(train_data)
+inputs_test, queries_test, answers_test = vectorize_stories(test_data)
+
+print(f"index of yes: {tokenizer.word_index['yes']}")
+print(f"index of no: {tokenizer.word_index['no']}")
+
+
