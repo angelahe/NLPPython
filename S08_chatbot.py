a55f4ae15dfd9e2f157f985851462d992a17c8e6
angela <angela@aviak.ca>
2021-01-31 16:10:29 -0700

    encode the data to prep for training

--- S08_chatbot.py
+++ S08_chatbot.py
@@ -131,4 +131,77 @@ inputs_test, queries_test, answers_test = vectorize_stories(test_data)
 print(f"index of yes: {tokenizer.word_index['yes']}")
 print(f"index of no: {tokenizer.word_index['no']}")
 
+# create the model
+from keras.models import Sequential, Model
+from keras.layers.embeddings import Embedding
+from keras.layers import Input, Activation, Dense, Permute, Dropout
+from keras.layers import add, dot, concatenate
+from keras.layers import LSTM
+
+input_sequence = Input((max_story_len,))
+question = Input((max_question_len,))
+
+# input encoder m
+# Input gets embedded to a sequence of vectors
+input_encoder_m = Sequential()
+input_encoder_m.add(Embedding(input_dim=vocab_size,output_dim=64))
+input_encoder_m.add(Dropout(0.3))
+
+# This encoder will output:
+# (samples, story_maxlen, embedding_dim)
+
+# input encoder c
+# embed the input into a sequence of vectors of size query_maxlen
+input_encoder_c = Sequential()
+input_encoder_c.add(Embedding(input_dim=vocab_size,output_dim=max_question_len))
+input_encoder_c.add(Dropout(0.3))
+# output: (samples, story_maxlen, query_maxlen)
+
+# question encoder
+# embed the question into a sequence of vectors
+question_encoder = Sequential()
+question_encoder.add(Embedding(input_dim=vocab_size,
+                               output_dim=64,
+                               input_length=max_question_len))
+question_encoder.add(Dropout(0.3))
+# output: (samples, query_maxlen, embedding_dim)
+
+# encode the sequences
+# encode input sequence and questions (which are indices)
+# to sequences of dense vectors
+input_encoded_m = input_encoder_m(input_sequence)
+input_encoded_c = input_encoder_c(input_sequence)
+question_encoded = question_encoder(question)
+
+# use dot product to compute the match between first input vector seq and the query
+# shape: `(samples, story_maxlen, query_maxlen)`
+match = dot([input_encoded_m, question_encoded], axes=(2, 2))
+match = Activation('softmax')(match)
+
+# add the match matrix with the second input vector sequence
+# add the match matrix with the second input vector sequence
+response = add([match, input_encoded_c])  # (samples, story_maxlen, query_maxlen)
+response = Permute((2, 1))(response)  # (samples, query_maxlen, story_maxlen)
+
+# concatenate the match matrix with the question vector sequence
+answer = concatenate([response, question_encoded])
+
+print(answer)
+
+# Reduce with RNN (LSTM)
+answer = LSTM(32)(answer)  # (samples, 32)
+
+# Regularization with Dropout
+answer = Dropout(0.5)(answer)
+answer = Dense(vocab_size)(answer)  # (samples, vocab_size)
+
+# we output a probability distribution over the vocabulary
+answer = Activation('softmax')(answer)
+
+# build the final model
+model = Model([input_sequence, question], answer)
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+print(model.summary)
 
